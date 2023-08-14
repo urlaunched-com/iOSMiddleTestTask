2edcb550155d322103a0f2c7dd623607b96ca533
Pavlo Chorney <chorney.pavel@gmail.com>
2023-08-14 17:09:29 +0300

    Initial Commit

---
+++ LaunchedDemo/LaunchedDemo/LaunchedDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchedDemoApp.swift
+//  LaunchedDemo
+//
+//  Created by Pasha Berger on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct LaunchedDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
